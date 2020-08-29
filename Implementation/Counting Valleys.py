2d8206ff0e52b8e69158d1b2d985ebda39538e9d
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2020-08-29 13:00:55 +0300

    11.0

---
+++ Implementation/Counting Valleys.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the countingValleys function below.
+def countingValleys(n, s):
+    altitude = valleys = 0
+
+    for step in s:
+        altitude += 1 if step == 'U' else -1
+        if altitude == 0 and step == 'U':
+            valleys += 1
+
+    return valleys
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    s = input()
+
+    result = countingValleys(n, s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
