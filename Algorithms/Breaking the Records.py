32c872c98d4662b51fc75035c227111c78c94d77
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2020-08-29 12:43:58 +0300

    8.0

---
+++ Algorithms/Breaking the Records.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the breakingRecords function below.
+def breakingRecords(scores):
+    min = max = scores[0]
+    min_count = max_count = 0
+    for i in scores[1:]:
+        if i > max:
+            max_count += 1
+            max = i
+        if i < min:
+            min_count += 1
+            min = i
+    return max_count, min_count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    scores = list(map(int, input().rstrip().split()))
+
+    result = breakingRecords(scores)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
