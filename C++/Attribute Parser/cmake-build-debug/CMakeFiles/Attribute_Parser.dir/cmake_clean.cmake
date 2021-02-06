1960804884ac5c95cc51c5163fc5a33fca9ebc1a
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2021-02-06 19:15:50 +0300

    5.0.1

---
+++ C++/Attribute Parser/cmake-build-debug/CMakeFiles/Attribute_Parser.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Attribute_Parser.exe"
+  "Attribute_Parser.exe.manifest"
+  "Attribute_Parser.lib"
+  "Attribute_Parser.pdb"
+  "CMakeFiles/Attribute_Parser.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Attribute_Parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
