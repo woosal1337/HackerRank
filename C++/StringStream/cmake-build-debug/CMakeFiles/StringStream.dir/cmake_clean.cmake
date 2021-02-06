138f03901aee7212ae104a6b9e8d3958acb00770
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2021-02-06 19:13:51 +0300

    5.0

---
+++ C++/StringStream/cmake-build-debug/CMakeFiles/StringStream.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StringStream.dir/main.cpp.obj"
+  "StringStream.exe"
+  "StringStream.exe.manifest"
+  "StringStream.lib"
+  "StringStream.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StringStream.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
